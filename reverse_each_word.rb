ecfaf1c51519eca0168288f1017b7ef109700256
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-19 02:18:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   reverse_sentence = []
   words_reversed = sentence.split(" ")
-  b = words_reversed.collect do |word|
+  words_reversed.collect do |word|
     reverse_sentence << word.reverse
   end
-  b.join(" ")
+  
 end
 
